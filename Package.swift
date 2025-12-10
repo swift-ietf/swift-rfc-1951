ff0cc541d1ad60014e7c7d06241489db37fbabb2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:27:57 +0100

    Update swift-standards to 0.10.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "RFC 1951", targets: ["RFC 1951"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0"),
     ],
     targets: [
         .target(
